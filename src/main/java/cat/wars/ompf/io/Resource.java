329985d3e852acb208ee1d2c213aec43bf684676
wars <huangrongfeng1999@gmail.com>
2020-04-03 12:39:03 +0800

    feat(all): Initial project

---
+++ src/main/java/cat/wars/ompf/io/Resource.java
@@ -0,0 +1,16 @@
+package cat.wars.ompf.io;
+
+import java.io.InputStream;
+
+/**
+ * @program: oh-my-pf
+ * @description: Reader configure file
+ * @author: Wars
+ * @created: 2020-04-02 01:20
+ */
+public class Resource {
+
+	public static InputStream getResourceAsStream(String path){
+		return ClassLoader.getSystemResourceAsStream("SQLMapConfig.yml");
+	}
+}
